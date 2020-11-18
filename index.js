ff2a95cd1bbbc86380124af363fed7761d963fa7
Willy Merlet <willymmerlet@gmail.com>
2020-11-18 12:11:08 +0100

    paym

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.post("/payment", async (req, res) => {
     const stripeToken = req.fields.stripeToken;
 
     const response = await stripe.charges.create({
-      amount: req.fields.amount * 100,
+      amount: 2004,
       currency: "eur",
-      description: req.fields.title,
+      description: "jdnsdd,n",
       source: stripeToken,
     });
     console.log(response);
